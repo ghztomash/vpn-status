78d0cd0c10ceda490d92a52bc5e1e5a1732b4080
Tomash Ghz <ghz.tomash@hotmail.com>
2024-03-19 21:25:43 +0100

    Load provided config file

--- vpn-status/src/config.rs
+++ vpn-status/src/config.rs
@@ -1,9 +1,10 @@
 use clap::Parser;
 use color_eyre::Result;
 use serde::{Deserialize, Serialize};
+use std::default::Default;
 use std::path::PathBuf;
 
-#[derive(Parser, Default, Debug, Serialize, Deserialize, Clone)]
+#[derive(Parser, Debug, Serialize, Deserialize, Clone)]
 #[command(version, about, long_about = None)]
 pub struct Config {
     #[arg(short, long)]
@@ -20,47 +21,60 @@ pub struct Config {
     pub config_path: Option<PathBuf>,
 }
 
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            no_color: false,
+            enabled_string: Some("enabled".to_string()),
+            enabled_color: Some("green".to_string()),
+            disabled_string: Some("disabled".to_string()),
+            disabled_color: Some("red".to_string()),
+            config_path: None,
+        }
+    }
+}
+
 impl Config {
-    pub fn load_config() -> Result<Self> {
-        let cfg: Config = confy::load("vpn_status", None)?;
-        let file = confy::get_configuration_file_path("vpn_status", None)?;
-        println!("The configuration file path is: {:#?}", file);
-        dbg!(&cfg);
-        Ok(cfg)
+    pub fn load_config(path: Option<PathBuf>) -> Result<Self> {
+        let config: Config = match path {
+            Some(path) => confy::load_path(path)?,
+            None => confy::load("vpn_status", Some("config"))?,
+        };
+        let file = confy::get_configuration_file_path("vpn_status", Some("config"))?;
+        println!("default configuration file is: {:#?}", file);
+        Ok(config)
     }
 
-    pub fn parse_args() -> Result<Self> {
-        let args = Config::parse();
-        dbg!(&args);
-        Ok(args)
+    pub fn parse_args() -> Self {
+        Config::parse()
     }
 
     pub fn get() -> Self {
         let mut config = Config::default();
-        if let Ok(config_file) = Config::load_config() {
+        // parse cli arguments
+        let args = Config::parse_args();
+        dbg!(&args);
+
+        // load config file
+        if let Ok(config_file) = Config::load_config(args.config_path) {
             config = config_file;
         }
+        dbg!(&config);
 
         // override config file with cli arguments
-        if let Ok(args_config) = Config::parse_args() {
-            if args_config.enabled_string.is_some() {
-                config.enabled_string = args_config.enabled_string;
-            }
-            if args_config.enabled_color.is_some() {
-                config.enabled_color = args_config.enabled_color;
-            }
+        if args.enabled_string.is_some() {
+            config.enabled_string = args.enabled_string;
+        }
+        if args.enabled_color.is_some() {
+            config.enabled_color = args.enabled_color;
+        }
 
-            if args_config.disabled_string.is_some() {
-                config.disabled_string = args_config.disabled_string;
-            }
+        if args.disabled_string.is_some() {
+            config.disabled_string = args.disabled_string;
+        }
 
-            if args_config.disabled_color.is_some() {
-                config.disabled_color = args_config.disabled_color;
-            }
-
-            if args_config.config_path.is_some() {
-                config.config_path = args_config.config_path;
-            }
+        if args.disabled_color.is_some() {
+            config.disabled_color = args.disabled_color;
         }
 
         config
