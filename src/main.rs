98854e36a2b086d9a9a1ca77ff6d38cf97fbc2d6
Tomash Ghz <ghz.tomash@hotmail.com>
2024-03-19 14:20:35 +0100

    cli tool

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
+use clap::Parser;
 use color_eyre::Result;
+use vpn_status_lib as vpn_status;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    boolean: bool,
+}
 
 fn main() -> Result<()> {
-    println!("-------------\ndefault_net:");
-    let default_interface = netdev::get_default_interface().unwrap();
-    println!(
-        "Interface: {:#?}  is_tun(): {}",
-        default_interface,
-        default_interface.is_tun()
-    );
+    let cli = Args::parse();
+
+    // println!("-------------\ndefault_net:");
+    // let default_interface = netdev::get_default_interface().unwrap();
+    // println!(
+    //     "Interface: {:#?}  is_tun(): {}",
+    //     default_interface,
+    //     default_interface.is_tun()
+    // );
 
     let status = vpn_status::get_status()?;
 
-    println!("vpn status: {}", status);
+    if cli.boolean {
+        println!("{}", vpn_status::vpn_enabled()?);
+    } else {
+        println!("{}", status);
+    }
 
     Ok(())
 }
