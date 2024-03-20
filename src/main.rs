684884f796e7a9ebc3ee616cd56c87cbf46fec46
Tomash Ghz <ghz.tomash@hotmail.com>
2024-03-20 18:21:39 +0100

    Renames types

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
+mod config;
+
 use color_eyre::Result;
 use colored::*;
-use vpn_status_lib::*;
-
-mod config;
 use config::Config;
+use vpn_status_lib::VpnStatus;
 
 fn main() -> Result<()> {
     // load the config from file or args
     let config = Config::get();
-    let status = get_status()?;
+    let status = vpn_status_lib::status()?;
 
     // get the custom status string if it exists
     let output: String = {
         let custom_status: Option<String> = match status {
-            Status::Enabled => config.clone().enabled_string,
-            Status::Disabled => config.clone().disabled_string,
+            VpnStatus::Enabled => config.clone().enabled_string,
+            VpnStatus::Disabled => config.clone().disabled_string,
         };
         custom_status.unwrap_or(format!("{}", status))
     };
@@ -24,8 +24,8 @@ fn main() -> Result<()> {
     } else {
         // get the custom color if it exists
         let custom_color = match status {
-            Status::Enabled => config.clone().enabled_color.unwrap_or("green".to_string()),
-            Status::Disabled => config.clone().disabled_color.unwrap_or("red".to_string()),
+            VpnStatus::Enabled => config.clone().enabled_color.unwrap_or("green".to_string()),
+            VpnStatus::Disabled => config.clone().disabled_color.unwrap_or("red".to_string()),
         };
         let color = colored::Color::from(custom_color);
         println!("{}", output.color(color));
--- vpn-status-lib/src/lib.rs
+++ vpn-status-lib/src/lib.rs
@@ -2,18 +2,18 @@ use std::fmt::Display;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum Error {
+pub enum VpnStatusError {
     #[error("Failed getting default interface")]
     DefaultInterface(String),
 }
 
 #[derive(Debug, PartialEq)]
-pub enum Status {
+pub enum VpnStatus {
     Disabled,
     Enabled,
 }
 
-impl Display for Status {
+impl Display for VpnStatus {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
             Self::Disabled => write!(f, "disabled"),
@@ -22,21 +22,21 @@ impl Display for Status {
     }
 }
 
-pub fn get_status() -> Result<Status, Error> {
+pub fn status() -> Result<VpnStatus, VpnStatusError> {
     match netdev::get_default_interface() {
         Ok(interface) => {
             if interface.is_tun() {
-                Ok(Status::Enabled)
+                Ok(VpnStatus::Enabled)
             } else {
-                Ok(Status::Disabled)
+                Ok(VpnStatus::Disabled)
             }
         }
-        Err(error) => Err(Error::DefaultInterface(error)),
+        Err(error) => Err(VpnStatusError::DefaultInterface(error)),
     }
 }
 
-pub fn vpn_enabled() -> Result<bool, Error> {
-    if get_status()? == Status::Enabled {
+pub fn vpn_enabled() -> Result<bool, VpnStatusError> {
+    if status()? == VpnStatus::Enabled {
         return Ok(true);
     }
     Ok(false)
@@ -48,7 +48,7 @@ mod tests {
 
     #[test]
     fn test_status() {
-        let result = get_status();
+        let result = status();
         assert!(result.is_ok());
     }
 
